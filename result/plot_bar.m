65c6bd68af68397b72b087ab972d74c3ba9a1906
He Zhang <hxzhang1@ualr.edu>
2020-11-05 23:11:19 -0500

    plot bar

---
+++ result/plot_bar.m
@@ -0,0 +1,15 @@
+function plot_bar()
+
+y = [1.7231992 0.0175880];
+y = y/2;
+bar(y);
+hold on; 
+% std = [0.59 0.006]; 
+% er = errorbar(y, std, std); 
+
+% er.Color = [0 0 0];                            
+% er.LineStyle = 'none'; 
+% hold off
+
+end
+
